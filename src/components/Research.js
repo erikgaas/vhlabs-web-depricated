92d14d406b82c5e21d413ced82b5a2fd70a34afe
Mr. Darkside <rachel@Mrs-MacBook-Pro.netgear.com>
2019-01-23 15:02:20 -0600

    Main Research page styling done

--- src/components/Research.js
+++ src/components/Research.js
@@ -5,52 +5,52 @@ class Research extends Component {
 	render() {
 		return (
 				<div className='container'>
-					<h1>Research</h1>
-					<h2>What would you like to learn about today?</h2>
+					<h1 id="researchHeader">Research</h1>
+					<h2 id="researchSubheader">What would you like to learn about today?</h2>
 
-					<div className="row">
-						<div className="col-md-3">
+					<div className="row" id="researchRow">
+						<div className="col-md-3" id="colFix">
 							<a href="research-3d">
-								<img src={'../images/research/icon3d.jpg'} height={250} width={250} alt={'3D Heart'}/>
-								<h3>3D Modeling</h3>
+								<img id="researchTopicImg" src={'../images/research/icon3d.jpg'} height={250} width={250} alt={'3D Heart'}/>
+								<h3 id="researchTopic">3D Modeling</h3>
 							</a>
 						</div>
 
-						<div className="col-md-3">
+						<div className="col-md-3" id="colFix">
 							<a href="research-ca">
-								<img src={'../images/research/iconcardiac.jpg'} height={250} width={250} alt={'Open Heart'}/>
-								<h3>Cardiac</h3>
+								<img id="researchTopicImg" src={'../images/research/iconcardiac.jpg'} height={250} width={250} alt={'Open Heart'}/>
+								<h3 id="researchTopic">Cardiac</h3>
 							</a>
 						</div>
 
-						<div className="col-md-3">
+						<div className="col-md-3" id="colFix">
 							<a href="research-pc">
-								<img src={'../images/research/iconhealth.jpg'} height={250} width={250} alt={'Testing Equipment'}/>
-								<h3>Patient Care</h3>
+								<img id="researchTopicImg" src={'../images/research/iconhealth.jpg'} height={250} width={250} alt={'Testing Equipment'}/>
+								<h3 id="researchTopic">Patient Care</h3>
 							</a>
 						</div>
 					</div>
 
 
-					<div className="row">
-						<div className="col-md-3">
+					<div className="row" id="researchRow">
+						<div className="col-md-3" id="colFix">
 							<a href="research-bb">
-								<img src={'../images/research/iconbear.jpg'} height={250} width={250} alt={'Black Bear Mom & Cub'}/>
-								<h3>Black Bears</h3>
+								<img id="researchTopicImg" src={'../images/research/iconbear.jpg'} height={250} width={250} alt={'Black Bear Mom & Cub'}/>
+								<h3 id="researchTopic">Black Bears</h3>
 							</a>
 						</div>
 
-						<div className="col-md-3">
+						<div className="col-md-3" id="colFix">
 							<a href="research-md">
-								<img src={'../images/research/icondevice.jpg'} height={250} width={250} alt={'Heart Monitor & Heart Model'}/>
-								<h3>Medical Devices</h3>
+								<img id="researchTopicImg" src={'../images/research/icondevice.jpg'} height={250} width={250} alt={'Heart Monitor & Heart Model'}/>
+								<h3 id="researchTopic">Medical Devices</h3>
 							</a>
 						</div>
 
-						<div className="col-md-3">
+						<div className="col-md-3" id="colFix">
 							<a href="research-sm">
-								<img src={'../images/research/iconsm.png'} height={250} width={250} alt={'Muscle Testing Equipment'}/>
-								<h3>Skeletal Muscle</h3>
+								<img id="researchTopicImg" src={'../images/research/iconsm.png'} height={250} width={250} alt={'Muscle Testing Equipment'}/>
+								<h3 id="researchTopic">Skeletal Muscle</h3>
 							</a>
 						</div>
 					</div>
