95b212a43f60df49fcf42fb37cd75a28f5d1c5d0
Michael Bateman <batem034@umn.edu>
2019-04-25 17:31:35 -0500

    remove link

--- src/components/ResearchTopic.js
+++ src/components/ResearchTopic.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
 import researchTopicsJson from "../json/researchTopics.json";
 
 var data = researchTopicsJson["research"];
